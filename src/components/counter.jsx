fda20bebd746c5a090a4b915afd375b2fa9d227a
Max Cheever <maxcheever8@hotmail.com>
2022-01-15 16:36:47 -0500

    added formatCount

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,21 @@ import React, { Component } from "react";
 
 class Counter extends Component {
     state = {
-        count: 0,
+        count: 0
     };
 
   render() {
     return (
       <React.Fragment>
-        <span>{this.state.count}</span>
+        <span>{this.formatCount()}</span>
         <button>Increment</button>
       </React.Fragment>
     );
+  }
+
+  formatCount() {
+      const { count } = this.state;
+      return count === 0 ? 'Zero' : count;
   }
 }
 
