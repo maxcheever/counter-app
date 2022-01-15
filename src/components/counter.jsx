1f91c91d80cf637a525a58866952341f7b72dd72
Max Cheever <maxcheever8@hotmail.com>
2022-01-15 18:28:09 -0500

    more formatting

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,31 +1,31 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-   state = {
-      count: 1,
-   };
+	state = {
+		count: 1,
+	};
 
-   render() {
-      let classes = this.getBadgeClasses();
+	render() {
+		let classes = this.getBadgeClasses();
 
-      return (
-         <div>
-            <span class={classes}>{this.formatCount()}</span>
-            <button class="btn btn-secondary btn-sm">Increment</button>
-         </div>
-      );
-   }
+		return (
+			<div>
+				<span class={classes}>{this.formatCount()}</span>
+				<button class="btn btn-secondary btn-sm">Increment</button>
+			</div>
+		);
+	}
 
-   getBadgeClasses() {
-      let classes = "badge m-2 bg-";
-      classes += this.state.count === 0 ? "warning" : "primary";
-      return classes;
-   }
+	getBadgeClasses() {
+		let classes = "badge m-2 bg-";
+		classes += this.state.count === 0 ? "warning" : "primary";
+		return classes;
+	}
 
-   formatCount() {
-      const { count } = this.state;
-      return count === 0 ? "Zero" : count;
-   }
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? "Zero" : count;
+	}
 }
 
 export default Counter;
