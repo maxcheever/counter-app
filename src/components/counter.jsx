9b3957a139f093dfd254c0039bfbc25b1b125bbd
Max Cheever <maxcheever8@hotmail.com>
2022-01-15 20:18:06 -0500

    4 Counters!!!

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,21 +6,10 @@ class Counter extends Component {
 		tags: [],
 	};
 
-	handleIncrement = () => {
+	handleIncrement = (product) => {
+		console.log(product);
 		this.setState({ count: this.state.count + 1 });
 	};
-
-	renderTags() {
-		if (this.state.tags.length === 0) return <p>There are no tags!</p>;
-
-		return (
-			<ul>
-				{this.state.tags.map((tag) => (
-					<li key={tag}>{tag}</li>
-				))}
-			</ul>
-		);
-	}
 
 	render() {
 		return (
@@ -29,12 +18,11 @@ class Counter extends Component {
 					{this.formatCount()}
 				</span>
 				<button
-					onClick={this.handleIncrement}
+					onClick={() => this.handleIncrement({ id: 1 })}
 					className="btn btn-secondary btn-sm"
 				>
 					Increment
 				</button>
-				{this.renderTags()}
 			</div>
 		);
 	}
