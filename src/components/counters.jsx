c5c2848fe486c0d2cd07540b9f1f85600fa26024
Max Cheever <maxcheever8@hotmail.com>
2022-01-15 21:24:28 -0500

    set state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -11,7 +11,13 @@ class Counters extends Component {
 		],
 	};
 
-	handleIncrement = (counter) => {};
+	handleIncrement = (counter) => {
+		const counters = [...this.state.counters];
+		const index = counters.indexOf(counter);
+		counters[index] = { ...counter };
+		counters[index].value++;
+		this.setState({ counters });
+	};
 
 	handleReset = () => {
 		const counters = this.state.counters.map((c) => {
